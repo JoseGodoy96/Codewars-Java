8877abe1cd7157170b19317f948224a7c2df5be7
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-30 23:44:52 +0100

    <feat>(Square.java): quinto ejercicio del nivel 8kyu

---
+++ 8kyu/Square.java
@@ -0,0 +1,12 @@
+
+/*
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+public class Square {
+	public static int square(int n) {
+       return n * n;
+    }
+}
