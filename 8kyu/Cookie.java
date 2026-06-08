6ec47452ec1c2d87164f4e89df9b17244047d1e9
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-08 20:49:40 +0100

    <feat>(Cookie.java): Finalizacion del ejercicio Cookie.java

---
+++ 8kyu/Cookie.java
@@ -0,0 +1,33 @@
+
+/*
+
+For this problem you must create a program that says who ate the last cookie.
+
+If the input is a string then "Zach" ate the cookie.
+
+If the input is a Double or an Integer then "Monica" ate the cookie.
+
+If the input is anything else "the dog" ate the cookie.
+
+The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+Please leave feedback for this kata. Cheers!
+
+*/
+
+public class Cookie {
+    public static String cookie(Object x) {
+
+		if (x instanceof String) {
+			return "Who ate the last cookie? It was Zach!";
+		} else if (x instanceof Integer || x instanceof Double) {
+			return "Who ate the last cookie? It was Monica!";
+		} else {
+			return "Who ate the last cookie? It was The dog!";
+		}
+    }
+}
